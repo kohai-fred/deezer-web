198d4317c6c7ad7fa1b6e32fcbe63cecdba182d9
kohai-fred <frederick.vu.pro@gmail.com>
2022-02-28 18:47:04 +0100

    add: Router View

--- src/main.js
+++ src/main.js
@@ -1,4 +1,17 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import { createRouter, createWebHistory } from "vue-router";
 
-createApp(App).mount('#app')
+const router = createRouter({
+    routes: [
+        { path: "/", component: () => import("./pages/Route1.vue") },
+        { path: "/route2", component: () => import("./pages/Route2View.vue") },
+    ],
+    history: createWebHistory(import.meta.env.BASE_URL),
+});
+
+const app = createApp(App);
+
+app.use(router);
+
+app.mount("#app");
---
+++ src/pages/Route1.vue
@@ -0,0 +1,4 @@
+<script setup></script>
+<template>
+    <h1>Route 1</h1>
+</template>
